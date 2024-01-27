c2546ef83d9ea59eb5204693f0b7872c1205829f
Dima Fomberg <dimaf@google.com>
2024-01-27 08:47:52 +0000

    Currenct exchange function removed due to no working library

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import WorldTime from 'react-world-time';
-import CurrencyExchange from 'react-currency-exchange';
 
 function App() {
   const [currencyRates, setCurrencyRates] = useState({});
@@ -16,17 +15,14 @@ function App() {
       <div>
         <h2>London</h2>
         <WorldTime timezone="Europe/London" />
-        <CurrencyExchange from="USD" to="GBP" rates={currencyRates} amount={1} />
       </div>
       <div>
         <h2>Tel Aviv</h2>
         <WorldTime timezone="Asia/Jerusalem" />
-        <CurrencyExchange from="USD" to="ILS" rates={currencyRates} amount={1} />
       </div>
       <div>
         <h2>Sunnyvale</h2>
         <WorldTime timezone="America/Los_Angeles" />
-        <CurrencyExchange from="USD" to="USD" rates={currencyRates} amount={1} />
       </div>
     </div>
   );
